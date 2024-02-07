f50825e3da9fac7af126d89ebeddaf6759cf096e
Police65 <65681291+Police65@users.noreply.github.com>
2024-02-07 00:02:37 -0400

    si

---
+++ el_puro_codigo_mientras_se_sube_el_proyecto/lib/utils/conversion_helper.dart
@@ -0,0 +1,13 @@
+class ConversionHelper {
+  static double metersToCentimeters(double meters) {
+    return meters * 100.0;
+  }
+
+  static double metersToFeet(double meters) {
+    return meters * 3.281;
+  }
+
+  static double metersToInches(double meters) {
+    return meters * 39.37;
+  }
+}
