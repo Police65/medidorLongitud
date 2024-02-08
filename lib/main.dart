9d99c27db5c4819ae656c754f291bd83f2a69337
Police65 <65681291+Police65@users.noreply.github.com>
2024-02-07 23:05:42 -0400

    V1.1.0
    Versión 1.1.0
    - Se agregó la funcionalidad de cambiar el color del texto a blanco en modo oscuro para mejorar la legibilidad.
    - Se corrigieron errores relacionados con la seguridad de los nulos en Dart. (literalmente aprendí eso hoy)
    - Se mejoró la lógica de conversión para soportar todas las combinaciones de unidades de medida.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class ConversionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Unit Conversion',
+      title: 'Unidad de conversion',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -26,13 +26,42 @@ class _ConversionPageState extends State<ConversionPage> {
   double _inputValue = 0.0;
   bool _isDarkMode = false;
   String _result = "";
+  String _startUnit = "Metros";
+  String _convertedUnit = "Centimetros";
 
   void _convert() {
     setState(() {
-      _result =
-          "Centimetros: ${ConversionHelper.metersToCentimeters(_inputValue)}\n"
-          "pies: ${ConversionHelper.metersToFeet(_inputValue)}\n"
-          "pulgadas: ${ConversionHelper.metersToInches(_inputValue)}";
+      double meters = 0.0;
+
+      switch (_startUnit) {
+        case 'Metros':
+          meters = _inputValue;
+          break;
+        case 'Centimetros':
+          meters = ConversionHelper.centimetersToMeters(_inputValue);
+          break;
+        case 'Pies':
+          meters = ConversionHelper.feetToMeters(_inputValue);
+          break;
+        case 'Pulgadas':
+          meters = ConversionHelper.inchesToMeters(_inputValue);
+          break;
+      }
+
+      switch (_convertedUnit) {
+        case 'Metros':
+          _result = '$meters m';
+          break;
+        case 'Centimetros':
+          _result = '${ConversionHelper.metersToCentimeters(meters)} cm';
+          break;
+        case 'Pies':
+          _result = '${ConversionHelper.metersToFeet(meters)} ft';
+          break;
+        case 'Pulgadas':
+          _result = '${ConversionHelper.metersToInches(meters)} in';
+          break;
+      }
     });
   }
 
@@ -48,6 +77,7 @@ class _ConversionPageState extends State<ConversionPage> {
       backgroundColor: _isDarkMode ? Colors.black : Colors.white,
       appBar: AppBar(
         title: Text('Unidad de conversion'),
+        backgroundColor: _isDarkMode ? Colors.grey[900] : Colors.blue,
         actions: <Widget>[
           IconButton(
             icon: Icon(_isDarkMode ? Icons.brightness_7 : Icons.brightness_3),
@@ -57,34 +87,82 @@ class _ConversionPageState extends State<ConversionPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  _inputValue = double.tryParse(value) ?? 0.0;
-                });
-              },
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Introduce el valor en metros',
+        child: Container(
+          decoration: BoxDecoration(
+            color: _isDarkMode ? Color(0xFF202225) : Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: _isDarkMode ? Color(0xFF37393f) : Color(0xFF37393f),
+            ),
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              DropdownButton<String>(
+                value: _startUnit,
+                style:
+                    TextStyle(color: _isDarkMode ? Colors.white : Colors.black),
+                items: <String>['Metros', 'Centimetros', 'Pies', 'Pulgadas']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _startUnit = newValue ?? _startUnit;
+                  });
+                },
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _convert,
-              child: Text('Convertir'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              _result,
-              style: TextStyle(
-                fontSize: 24,
-                color: _isDarkMode ? Colors.white : Colors.black,
+              DropdownButton<String>(
+                value: _convertedUnit,
+                style:
+                    TextStyle(color: _isDarkMode ? Colors.white : Colors.black),
+                items: <String>['Metros', 'Centimetros', 'Pies', 'Pulgadas']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _convertedUnit = newValue ?? _convertedUnit;
+                  });
+                },
               ),
-            ),
-          ],
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _inputValue = double.tryParse(value) ?? 0.0;
+                  });
+                },
+                style:
+                    TextStyle(color: _isDarkMode ? Colors.white : Colors.black),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Ingresa el valor en $_startUnit',
+                  labelStyle: TextStyle(
+                      color: _isDarkMode ? Colors.white : Colors.black),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _convert,
+                child: Text('Convertir'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                _result,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: _isDarkMode ? Colors.white : Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/conversion_helper.dart
+++ lib/utils/conversion_helper.dart
@@ -10,4 +10,16 @@ class ConversionHelper {
   static double metersToInches(double meters) {
     return meters * 39.37;
   }
+
+  static double centimetersToMeters(double centimeters) {
+    return centimeters / 100.0;
+  }
+
+  static double feetToMeters(double feet) {
+    return feet / 3.281;
+  }
+
+  static double inchesToMeters(double inches) {
+    return inches / 39.37;
+  }
 }
